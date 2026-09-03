becbe3ac242034297a2055ae0c2b0351178689f8
Muhammad Anas <anasthedeveloper555@gmail.com>
2026-09-03 18:11:55 +0500

    Selective delete: removed 51 files

--- app/src/main/java/com/anas/linkchain/service/TargetDownloaderConfig.kt
+++
@@ -1,9 +0,0 @@
-package com.anas.linkchain.service
-
-object TargetDownloaderConfig {
-    const val DEFAULT_PACKAGE = "com.example.videodownloader"
-    const val INPUT_FIELD_ID = "com.example.videodownloader:id/et_url_input"
-    const val QUALITY_DIALOG_BUTTON_ID = "com.example.videodownloader:id/btn_quality_select"
-    const val CONFIRM_DOWNLOAD_BUTTON_ID = "com.example.videodownloader:id/btn_start_download"
-    const val NOTIF_COMPLETION_PHRASE = "download complete"
-}
